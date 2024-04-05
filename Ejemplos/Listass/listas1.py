3f886839fbe20f3359e19be727af8e82183c992a
menene <efmarroquinr@gmail.com>
2024-04-05 13:51:25 -0600

    litas

---
+++ Ejemplos/Listass/listas1.py
@@ -0,0 +1,60 @@
+lista1 = [2, 2, 3, 4, 5, 6]
+lista2 = [9, 8, 7]
+
+# largo = len(lista1)
+# print(largo, len(lista2))
+
+# print(lista1 + lista2)
+# print(lista2 * 4)
+
+# cadena = "oola"
+# cadena[0] = "H"
+
+# MODIFICAR VALORES DE UNA LISTA
+# print(lista1)
+# lista1[0] = 1
+# print(lista1)
+
+# AGREGAR ELEMENTOS A UNA LISTA
+lista3 = [2, 3, 4, 5, 6]
+# print("ORIGINAL:", lista3)
+# lista3.append(7)
+# print("APPEND:",lista3)
+# lista3.insert(0, 1)
+# print("INSERT:",lista3)
+
+# ELIMINAR ELEMENTOS DE UNA LISTA
+# del lista3[0]
+# print(lista3)
+# 
+# lista3.remove(7)
+# print(lista3)
+
+# DA ERROR!!!!
+# lista3.remove(79988)
+# print(lista3)
+
+# numero = lista3.pop()
+# print(lista3)
+# print(numero)
+# 
+# lista3.clear()
+# print(lista3)
+
+# ORDENAR UNA LISTA
+# lista3 = [100, 169, 27, 229, 310]
+# print(lista3)
+# lista3.sort()
+# print(lista3)
+# lista3.sort(reverse = True)
+# print(lista3)
+
+# CONVERTIR ENTRE CADENAS Y LISTAS
+cadena = "Programar es divertido"
+palabras = cadena.split(" ")
+print(palabras)
+nueva_cadena = "-".join(palabras)
+print(nueva_cadena)
+
+
+
